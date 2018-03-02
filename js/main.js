90b278bc469219226507b54c0a914fb9e4e1983f
linchangyi <talentlcy@hotmail.com>
2018-03-02 09:32:57 +0800

    Site updated: 2018-03-02 09:32:55

--- js/main.js
+++ js/main.js
@@ -247,7 +247,15 @@
             }
         },
         autoHideHeader: function(){
-            var headroom  = new Headroom(header);
+            var headroom = new Headroom(header, {
+                "offset": 205,
+                "tolerance": 5,
+                "classes": {
+                  "initial": "animated",
+                  "pinned": "slideDown",
+                  "unpinned": "slideUp"
+                }
+              });
             headroom.init();   
         },
         reward: function () {
